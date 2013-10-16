8b731ba77388261013722cbc97c6c7794414541a
Hector Dearman <hector.dearman@gmail.com>
2013-10-16 19:32:04 +0100

    my setup

--- Deeva/trial.py
+++ Deeva/trial.py
@@ -1,4 +1,26 @@
+import os
+
+# add this folder to classpath
+classpath = os.environ.get("CLASSPATH", "")
+os.environ["CLASSPATH"] = os.path.dirname('.') + os.pathsep + classpath
+print os.environ["CLASSPATH"]
+
+#path = os.environ.get("CLASSPATH", "")
+#os.environ["CLASSPATH"] = os.path.dirname(os.path.abspath(__file__)) + os.pathsep + classpath
+#print os.environ["CLASSPATH"]
+
+
 from jnius import autoclass
+
+System = autoclass('java.lang.System')
+print System.getProperty('java.class.path')
+
+Stack = autoclass('java.util.Stack')
+stack = Stack()
+stack.push('hello')
+stack.push('world')
+print stack.pop()
+print stack.pop()
 
 MyTrial = autoclass('MyTrial')
 unicorn = MyTrial()
