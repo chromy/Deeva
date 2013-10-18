10dff30228febbc42b646669eefc0f698864da3e
Kritaphat Songsri-in <kritaphat.songsri-in11@imperial.ac.uk>
2013-10-18 16:06:46 +0100

    [FRONTEND] update controller and index to show code field

--- deeva/static/js/controllers.js
+++ deeva/static/js/controllers.js
@@ -5,27 +5,38 @@ deeva.controller('SimpleController', function ($scope, $http) {
     .success(function(data) {
       $scope.codeName = data.codeName;
       $scope.code = data.code;
-      codeMirror = CodeMirror(document.getElementById('codeInputPane'), {
-        value: $scope.code,
-        mode: 'text/x-java',
-        lineNumbers: true,
-        tabSize: 4,
-        indentUnit: 4,
-        iewportMargin: 0,
-        readOnly: "nocursor",
-        extraKeys: {
-          Tab: function(cm) {
-            var lo = cm.getCursor("start").line;
-            var hi = cm.getCursor("end").line;
-            for (var i = lo; i <= hi; i++)
-              cm.indentLine(i, "smart");
-            cm.setCursor(cm.getCursor("end"));
-            return true;
-          }
-        }
-      });
+      displayCode($scope);
     })
-    .error(function(data, status) {
+    .error(function(status) {
+      $scope.codeName = "Can not load Java code";
+      $scope.code = [];
       console.log("There is an error getting json");
-  });  
+      displayCode($scope);
+  });
+
 });
+
+function displayCode($scope) {
+  codeMirror = CodeMirror(document.getElementById('codeInputPane'), {
+  mode: 'text/x-java',
+  lineNumbers: true,
+  tabSize: 4,
+  indentUnit: 4,
+  iewportMargin: 0,
+  readOnly: "nocursor",
+  extraKeys: {
+    Tab: function(cm) {
+      var lo = cm.getCursor("start").line;
+      var hi = cm.getCursor("end").line;
+      for (var i = lo; i <= hi; i++) {
+        cm.indentLine(i, "smart");
+      }
+      cm.setCursor(cm.getCursor("end"));
+      return true;
+    }
+  }
+  });
+  for (index = 0;index < $scope.code.length;index++) {
+    codeMirror.setLine(index, $scope.code[index]);
+  }
+}
