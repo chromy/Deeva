53a22708586e8e45908a9b18d2ad81a7af717a23
Hector Dearman <hector.dearman@gmail.com>
2013-10-25 01:14:01 +0100

    added open window flag to run_deeva

--- run_deeva.py
+++ run_deeva.py
@@ -3,18 +3,23 @@
 import os, webbrowser
 from deeva import app, debug
 
-def main(prog):
+def main(prog, open_browser=False):
     # Start the Java debug server
     classpath = os.path.dirname(os.path.abspath(__file__)) + "/deeva"
     app.debugger = debug.create_java_debugger(classpath)
 
+    # Open the browser if desired
+    if open_browser:
+        webbrowser.open('http://localhost:5000')
+
     # Start Flask
-    webbrowser.open('http://localhost:5000')
     app.run()
 
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("prog", help="the program you want to debug")
+    parser.add_argument('-s', '--suppress', dest='open_browser', action='store_false', help="do not open browser window")
     args = parser.parse_args()
-    main(args.prog)
+    main(args.prog, args.open_browser)
+
