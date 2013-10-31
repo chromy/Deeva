f0fe898381baa5321520448b1c3ed8990aac1696
Kritaphat Songsri-in <kritaphat.songsri-in11@imperial.ac.uk>
2013-10-31 19:33:16 +0000

    rearrage control.js

--- deeva/static/js/controllers.js
+++ deeva/static/js/controllers.js
@@ -11,84 +11,8 @@ deeva.controller('SimpleController', function ($scope, $http) {
   $scope.currentPrompt = "";
 
   $(".resizable").resizable();
-
-  loadMainClass($scope, $http);
-
-  // Invoke a GET method to ask for Java code.
-  function loadMainClass($scope, $http) {
-    $http.get('./main_class.json')
-      .success(function(data) {
-        $scope.file_name = data.file_name;
-        $scope.code = data.code;
-        $scope.displayCode();
-      })
-      .error(function(status) {
-        $scope.file_name = "Can not load Java code";
-        $scope.code = [];
-        console.log("There is an error getting json");
-        $scope.displayCode();
-    });
-  };
-
-  // Initialze codeMirror and display it
-  $scope.displayCode = function() {
-    // Initialize codeMirror
-    $scope.codeMirror = CodeMirror(document.getElementById('codeInputPane'), {
-      mode: 'text/x-java',
-      tabSize: 4,
-      lineNumbers: true,
-      lineWrapping: true,
-      firstLineNumber: 0,
-      readOnly: "nocursor",
-      gutters: ["CodeMirror-linenumbers", "breakpoints"],
-    });
-    // Set an event handler when the gutter is clicked
-    // which update frontend as well ass invoke a method setBreakPoints
-    $scope.codeMirror.on("gutterClick", function(cm, line) {
-      var info = cm.lineInfo(line);
-      var breakPoint;
-      if (info.gutterMarkers) {
-        breakPoint = null;
-        $scope.breakPoints.splice($scope.breakPoints.indexOf(line), 1);
-      } else {
-        breakPoint = $scope.makeBreakPoint();
-        $scope.breakPoints.push(line);
-      }
-      cm.setGutterMarker(line, "breakpoints", breakPoint);
-      $scope.setBreakPoints();
-    });
-    // Load an input file to be display in codeMirror
-    for (index = 0;index < $scope.code.length;index++) {
-      $scope.codeMirror.setLine(index, $scope.code[index]);
-    }
-    $scope.codeMirror.setCursor($scope.codeMirror.firstLine());
-  }
-
-  // Invoke a POST method to backend to send a data about a set of breakpoint.
-  $scope.setBreakPoints = function() {
-    $http.post('breakPoints', $scope.breakPoints)
-      .success(function(data) {
-        //console.log(data.status);
-      })
-      .error(function(data) {
-        console.log("There is an error sending break points " + data.status);
-    });
-  }
-
-  // Return a div that contain a marker for breakpoint.
-  $scope.makeBreakPoint = function () {
-    var breakPoint = document.createElement("div");
-    breakPoint.style.color = "#0000FF";
-    breakPoint.innerHTML = "●";
-    return breakPoint;
-  }
-
-  // High light the current line. The previous line high light is also removed.
-  $scope.highLightLine = function() {
-    var BACK_CLASS = "CodeMirror-activeline-background";
-    $scope.codeMirror.removeLineClass($scope.prevLine, "background", BACK_CLASS);
-    $scope.codeMirror.addLineClass($scope.currentLine, 'background', BACK_CLASS);
-  }
+  displayCodeMirror($scope, $http);
+  displayTerminal($scope);
 
   // Called by step button which send a POST method to backend infroming step occur
   $scope.step = function() {
@@ -99,7 +23,7 @@ deeva.controller('SimpleController', function ($scope, $http) {
           $scope.currentLine = data.step_number;
           $scope.highLightLine();
           $scope.codeMirror.setCursor($scope.currentLine);
-          $scope.printToTerminal(data.stdout);
+          printToTerminal($scope, data.stdout);
           console.log("The current step is " + $scope.currentLine);
         })
         .error(function(status) {
@@ -108,6 +32,8 @@ deeva.controller('SimpleController', function ($scope, $http) {
       });
     }
   }
+
+  // Called by a run button which sned a POST method to backend to invoke run
   $scope.run = function() {
     $http.post('run')
       .success(function(data) {
@@ -118,10 +44,102 @@ deeva.controller('SimpleController', function ($scope, $http) {
       });
   };
 
-  $scope.displayTerminal = function() {
+  // Invoke a GET method to ask for Java code.
+  function displayCodeMirror($scope, $http) {
+    $http.get('./main_class.json')
+      .success(function(data) {
+        $scope.file_name = data.file_name;
+        $scope.code = data.code;
+        setUpCodeMirror($scope);
+      })
+      .error(function(status) {
+        $scope.file_name = "Can not load Java code";
+        $scope.code = [];
+        console.log("There is an error getting json");
+        setUpCodeMirror($scope);
+    });
+  };
+
+  // Initialze codeMirror and display it
+  function setUpCodeMirror($scope) {
+    //Initialize codeMirror
+    $scope.codeMirror = CodeMirror(document.getElementById('codeInputPane'), {
+      mode: 'text/x-java',
+      tabSize: 4,
+      lineNumbers: true,
+      lineWrapping: true,
+      firstLineNumber: 0,
+      readOnly: "nocursor",
+      gutters: ["CodeMirror-linenumbers", "breakpoints"],
+    });
+    // Load an input file to be display in codeMirror
+    for (index = 0;index < $scope.code.length;index++) {
+      $scope.codeMirror.setLine(index, $scope.code[index]);
+    }
+    $scope.codeMirror.setCursor($scope.codeMirror.firstLine());
+    setGutterHandler($scope);
+  }
+
+  // Set an event handler when the gutter is clicked
+  // which update frontend as well ass invoke a method setBreakPoints
+  function setGutterHandler($scope) {
+      $scope.codeMirror.on("gutterClick", function(cm, line) {
+      var info = cm.lineInfo(line);
+      var breakPoint;
+      if (info.gutterMarkers) {
+        breakPoint = null;
+        $scope.breakPoints.splice($scope.breakPoints.indexOf(line), 1);
+      } else {
+        breakPoint = makeBreakPoint();
+        $scope.breakPoints.push(line);
+      }
+      cm.setGutterMarker(line, "breakpoints", breakPoint);
+      setBreakPoints($scope, $http);
+    });
+  }
+
+  // Return a div that contain a marker for breakpoint.
+  function makeBreakPoint() {
+    var breakPoint = document.createElement("div");
+    breakPoint.style.color = "#0000FF";
+    breakPoint.innerHTML = "●";
+    return breakPoint;
+  }
+
+  // Invoke a POST method to backend to send a data about a set of breakpoint.
+  function setBreakPoints($scope, $http) {
+    $http.post('breakPoints', $scope.breakPoints)
+      .success(function(data) {
+        //console.log(data.status);
+      })
+      .error(function(data) {
+        console.log("There is an error sending break points " + data.status);
+    });
+  }
+
+  // High light the current line. The previous line high light is also removed.
+  function highLightLine($scope) {
+    var BACK_CLASS = "CodeMirror-activeline-background";
+    $scope.codeMirror.removeLineClass($scope.prevLine, "background", BACK_CLASS);
+    $scope.codeMirror.addLineClass($scope.currentLine, 'background', BACK_CLASS);
+  }
+
+    function printToTerminal($scope, output) {
+    if (output.slice(-1) == "\n") {
+      output = output.substring(0, output.length - 1);
+      var remainedToPrint = $scope.currentPrompt.substring(0, $scope.currentPrompt.length);
+      $scope.terminal.echo(remainedToPrint + output);
+      $scope.terminal.set_prompt(initial_prompt);
+    } else {
+      $scope.currentPrompt += (output);
+      $scope.terminal.set_prompt($scope.currentPrompt + initial_prompt);
+    }
+  }
+
+  function displayTerminal($scope) {
     $scope.terminal = $('#terminal').terminal(function(input, term) {
       // This function is called whenever the enter is hit.
-      $scope.sendInput(input);
+      sendInput($scope, input);
       }, {
         // Initial setup for
         greetings: "Welcome to Deeva",
@@ -132,27 +150,13 @@ deeva.controller('SimpleController', function ($scope, $http) {
     );
   }
 
-  $scope.sendInput = function(input) {
+  function sendInput($scope, input) {
     $http.post('input', input)
       .success(function(data) {
       })
       .error(function(data) {
         console.log("There is an error sending input " + data.status);
     });
-  }
-
-  $scope.displayTerminal();
-
-  $scope.printToTerminal = function(output) {
-    if (output.slice(-1) == "\n") {
-      output = output.substring(0, output.length - 1);
-      var remainedToPrint = $scope.currentPrompt.substring(0, $scope.currentPrompt.length);
-      $scope.terminal.echo(remainedToPrint + output);
-      $scope.terminal.set_prompt(initial_prompt);
-    } else {
-      $scope.currentPrompt += (output);
-      $scope.terminal.set_prompt($scope.currentPrompt + initial_prompt);
-    }
   }
 
 });
