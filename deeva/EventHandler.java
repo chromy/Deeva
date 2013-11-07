ebb12b25be6c2ca4215b4f0254af34c032a7ab9e
Felix de Souza <fd11@imperial.ac.uk>
2013-11-07 22:11:27 +0000

    Got printing state of stack to stdin, need to get maps through.

--- deeva/EventHandler.java
+++ deeva/EventHandler.java
@@ -1,7 +1,12 @@
 package deeva;
 
+import com.sun.jdi.*;
+import com.sun.jdi.connect.*;
+import com.sun.jdi.request.*;
 import com.sun.jdi.event.*;
 
 interface EventHandler {
-      void handleEvent(Event event);
+      void handleEvent(Event event) 
+	  throws IncompatibleThreadStateException, AbsentInformationException, 
+		 ClassNotLoadedException;
 }
