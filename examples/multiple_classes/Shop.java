ce0a7e4fc720de30360575cfc217468108a20e76
Xueqi Chen <xc1611@ic.ac.uk>
2013-11-12 21:15:57 +0000

    added a new multiple class example

---
+++ examples/multiple_classes/Shop.java
@@ -0,0 +1,9 @@
+package multiple_classes;
+
+public class Shop {
+    public static void main(String args[]) {
+	Book book = new Book();
+	double price = book.getPrice();
+	System.out.println("The book has a price of: "+price);
+    }
+}
