2264793ff1dcc142e62c5afe8cf6a630176fb2c6
Hector Dearman <hector.dearman@gmail.com>
2013-11-12 14:15:45 +0000

    white space

--- deeva/Debug.java
+++ deeva/Debug.java
@@ -67,16 +67,17 @@ public class Debug extends EventHandlerBase {
 
         EventRequestManager reqMgr = vm.eventRequestManager();
 
-        entryRequest = reqMgr.createMethodEntryRequest();
-        for (String ex: excludes) { entryRequest.addClassExclusionFilter (ex); }
-        entryRequest.setSuspendPolicy(EventRequest.SUSPEND_ALL);    // suspend so we can examine vars
-        //entryRequest.enable();
+        //entryRequest = reqMgr.createMethodEntryRequest();
+        //for (String ex: excludes) { entryRequest.addClassExclusionFilter (ex); }
+        //entryRequest.setSuspendPolicy(EventRequest.SUSPEND_ALL);    // suspend so we can examine vars
+        ////entryRequest.enable();
 
-        exitRequest = reqMgr.createMethodExitRequest();
-        for (String ex: excludes) { exitRequest.addClassExclusionFilter (ex); }
-        exitRequest.setSuspendPolicy(EventRequest.SUSPEND_ALL);    // suspend so we can examine vars
-        //exitRequest.enable();
-        //
+        //exitRequest = reqMgr.createMethodExitRequest();
+        //for (String ex: excludes) { exitRequest.addClassExclusionFilter (ex); }
+        //exitRequest.setSuspendPolicy(EventRequest.SUSPEND_ALL);    // suspend so we can examine vars
+        ////exitRequest.enable();
+        ////
+
         ClassPrepareRequest prepareRequest = reqMgr.createClassPrepareRequest();
         for (String ex: excludes) { prepareRequest.addClassExclusionFilter (ex); }
         prepareRequest.setSuspendPolicy(EventRequest.SUSPEND_ALL);    // suspend so we can examine vars
@@ -85,102 +86,102 @@ public class Debug extends EventHandlerBase {
     }
 
     public List<Map<String, String>> getStack(LocatableEvent event)
-    	throws IncompatibleThreadStateException, AbsentInformationException,
-	       ClassNotLoadedException
+        throws IncompatibleThreadStateException, AbsentInformationException,
+           ClassNotLoadedException
     {
-	Map<String, String> stack = new HashMap<String, String>();
-	/* Try to extract stack variables - Hack */
-	/* Get the thread in which we're stepping */
-	ThreadReference threadRef = event.thread();
+    Map<String, String> stack = new HashMap<String, String>();
+    /* Try to extract stack variables - Hack */
+    /* Get the thread in which we're stepping */
+    ThreadReference threadRef = event.thread();
 
-	/* Get the top most strack frame in the thread that we've stopped in */
-	StackFrame stackFrame = threadRef.frame(0);
+    /* Get the top most strack frame in the thread that we've stopped in */
+    StackFrame stackFrame = threadRef.frame(0);
 
-	/* We want to create a list of maps */
-	List<Map<String, String>> localVariables = new LinkedList<Map<String, String>>();
+    /* We want to create a list of maps */
+    List<Map<String, String>> localVariables = new LinkedList<Map<String, String>>();
 
-	/* List all the variables on the stack */
-	for (LocalVariable var : stackFrame.visibleVariables()) {
-	    Map<String, String> varMap = new HashMap<String, String>();
+    /* List all the variables on the stack */
+    for (LocalVariable var : stackFrame.visibleVariables()) {
+        Map<String, String> varMap = new HashMap<String, String>();
 
-	    String name = var.name();
-	    Type type = var.type();
-	    String typeString = var.typeName();
-	    Value variableValue = stackFrame.getValue(var);
-	    /*System.err.println("Type string: " + typeString);
-	    System.err.println("Type instance: " + type.getClass().getName());*/
-	    System.err.println("-------------");
-	    System.err.println("Name: " + name);
-	    System.err.println("Type: " + typeString);
-	    System.err.println("ValueType: " + variableValue.type());
-	    Type valueType = variableValue.type();
+        String name = var.name();
+        Type type = var.type();
+        String typeString = var.typeName();
+        Value variableValue = stackFrame.getValue(var);
+        /*System.err.println("Type string: " + typeString);
+        System.err.println("Type instance: " + type.getClass().getName());*/
+        System.err.println("-------------");
+        System.err.println("Name: " + name);
+        System.err.println("Type: " + typeString);
+        System.err.println("ValueType: " + variableValue.type());
+        Type valueType = variableValue.type();
 
-	    /* Insert local variable meta into a map that will get converted later */
-	    varMap.put("name", var.name());
-	    varMap.put("type", typeString);
+        /* Insert local variable meta into a map that will get converted later */
+        varMap.put("name", var.name());
+        varMap.put("type", typeString);
 
-	    if (valueType instanceof IntegerType) {
-		System.err.println("Value: " + ((IntegerValue)variableValue).value());
-		Integer value = ((IntegerValue)variableValue).value();
-		varMap.put("value", value.toString());
-	    } else if (valueType instanceof BooleanType) {
-		System.err.println("Value: " + ((BooleanValue)variableValue).value());
-		Boolean value = new Boolean(((BooleanValue)variableValue).value());
-		varMap.put("value", value.toString());
-	    } else if (valueType instanceof ByteType) {
-		System.err.println("Value: " + ((ByteValue)variableValue).value());
-		Byte value = new Byte(((ByteValue)variableValue).value());
-		varMap.put("value", value.toString());
-	    } else if (valueType instanceof CharType) {
-		System.err.println("Value: " + ((CharValue)variableValue).value());
-		Character value = new Character(((CharValue)variableValue).value());
-		varMap.put("value", value.toString());
-	    } else if (valueType instanceof DoubleType) {
-		System.err.println("Value: " + ((DoubleValue)variableValue).value());
-		Double value = new Double(((DoubleValue)variableValue).value());
-		varMap.put("value", value.toString());
-	    } else if (valueType instanceof FloatType) {
-		System.err.println("Value: " + ((FloatValue)variableValue).value());
-		Float value = new Float(((FloatValue)variableValue).value());
-		varMap.put("value", value.toString());
-	    } else if (valueType instanceof LongType) {
-		System.err.println("Value: " + ((LongValue)variableValue).value());
-		Long value = new Long(((LongValue)variableValue).value());
-		varMap.put("value", value.toString());
-	    } else if (valueType instanceof ShortType) {
-		System.err.println("Value: " + ((ShortValue)variableValue).value());
-		Short value = new Short(((ShortValue)variableValue).value());
-		varMap.put("value", value.toString());
-	    } else if (valueType instanceof VoidType) {
-		System.err.println("Value: void");
-		varMap.put("value", "void");
-	    }
+        if (valueType instanceof IntegerType) {
+        System.err.println("Value: " + ((IntegerValue)variableValue).value());
+        Integer value = ((IntegerValue)variableValue).value();
+        varMap.put("value", value.toString());
+        } else if (valueType instanceof BooleanType) {
+        System.err.println("Value: " + ((BooleanValue)variableValue).value());
+        Boolean value = new Boolean(((BooleanValue)variableValue).value());
+        varMap.put("value", value.toString());
+        } else if (valueType instanceof ByteType) {
+        System.err.println("Value: " + ((ByteValue)variableValue).value());
+        Byte value = new Byte(((ByteValue)variableValue).value());
+        varMap.put("value", value.toString());
+        } else if (valueType instanceof CharType) {
+        System.err.println("Value: " + ((CharValue)variableValue).value());
+        Character value = new Character(((CharValue)variableValue).value());
+        varMap.put("value", value.toString());
+        } else if (valueType instanceof DoubleType) {
+        System.err.println("Value: " + ((DoubleValue)variableValue).value());
+        Double value = new Double(((DoubleValue)variableValue).value());
+        varMap.put("value", value.toString());
+        } else if (valueType instanceof FloatType) {
+        System.err.println("Value: " + ((FloatValue)variableValue).value());
+        Float value = new Float(((FloatValue)variableValue).value());
+        varMap.put("value", value.toString());
+        } else if (valueType instanceof LongType) {
+        System.err.println("Value: " + ((LongValue)variableValue).value());
+        Long value = new Long(((LongValue)variableValue).value());
+        varMap.put("value", value.toString());
+        } else if (valueType instanceof ShortType) {
+        System.err.println("Value: " + ((ShortValue)variableValue).value());
+        Short value = new Short(((ShortValue)variableValue).value());
+        varMap.put("value", value.toString());
+        } else if (valueType instanceof VoidType) {
+        System.err.println("Value: void");
+        varMap.put("value", "void");
+        }
 
-	    /* Let's deal with Object references */
-	    else if (variableValue instanceof ObjectReference) {
-		/* This is guaranteed to be unique iff the object hasn't been
-		 * disposed of. Not too sure what the implications of this is
-		 * for us. */
-		Long uniqueID = ((ObjectReference)variableValue).uniqueID();
-		System.err.println("UniqueID: " + uniqueID);
-		varMap.put("refID", uniqueID.toString());
+        /* Let's deal with Object references */
+        else if (variableValue instanceof ObjectReference) {
+        /* This is guaranteed to be unique iff the object hasn't been
+         * disposed of. Not too sure what the implications of this is
+         * for us. */
+        Long uniqueID = ((ObjectReference)variableValue).uniqueID();
+        System.err.println("UniqueID: " + uniqueID);
+        varMap.put("refID", uniqueID.toString());
 
-		if (variableValue instanceof StringReference) {
-		    varMap.put("value", ((StringReference)variableValue).value());
-		} else if (variableValue instanceof ArrayReference) {
-		    Integer length = ((ArrayReference)variableValue).length();
-		    varMap.put("length", length.toString());
-		    ArrayType arrType = (ArrayType)valueType;
-		    System.err.println("length: " + length);
-		    System.err.println("component type: " + arrType.componentTypeName());
-		}
-	    }
+        if (variableValue instanceof StringReference) {
+            varMap.put("value", ((StringReference)variableValue).value());
+        } else if (variableValue instanceof ArrayReference) {
+            Integer length = ((ArrayReference)variableValue).length();
+            varMap.put("length", length.toString());
+            ArrayType arrType = (ArrayType)valueType;
+            System.err.println("length: " + length);
+            System.err.println("component type: " + arrType.componentTypeName());
+        }
+        }
 
-	    /* Append the local variable to the end of the list (stack) */
-	    localVariables.add(varMap);
-	}
+        /* Append the local variable to the end of the list (stack) */
+        localVariables.add(varMap);
+    }
 
-	return localVariables;
+    return localVariables;
     }
 
     public Map<String, Object> run() throws InterruptedException {
@@ -194,7 +195,7 @@ public class Debug extends EventHandlerBase {
         Map<String, Object> result = new HashMap<String, Object>();
         result.put("state", state);
         result.put("line_number", line_number);
-	result.put("stack", stack);
+    result.put("stack", stack);
         return result;
     }
 
@@ -326,8 +327,8 @@ public class Debug extends EventHandlerBase {
 
     @Override
     public void handleEvent(Event e)
-	throws IncompatibleThreadStateException, AbsentInformationException,
-	       ClassNotLoadedException
+    throws IncompatibleThreadStateException, AbsentInformationException,
+           ClassNotLoadedException
     {
         System.err.println(e.getClass());
         if (e instanceof LocatableEvent) {
@@ -382,13 +383,13 @@ public class Debug extends EventHandlerBase {
 
     @Override
     public void stepEvent(StepEvent event)
-	throws IncompatibleThreadStateException, AbsentInformationException,
-	       ClassNotLoadedException
+    throws IncompatibleThreadStateException, AbsentInformationException,
+           ClassNotLoadedException
     {
         System.err.println(event.location().method() + "@" + event.location().lineNumber());       
 
-	stack = getStack(event);
-	/* Delete the request */
+    stack = getStack(event);
+    /* Delete the request */
         EventRequestManager mgr = vm.eventRequestManager();
         mgr.deleteEventRequest(event.request());
         sema.release();
@@ -404,7 +405,7 @@ public class Debug extends EventHandlerBase {
         //    stepRequest = null;
         //}
 
-	/* Try to extract the stack variables */
+        /* Try to extract the stack variables */
 
         state = State.STASIS;
         sema.release();
