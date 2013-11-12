200515b100c228a57130762a72111e07e055ee4e
Xueqi Chen <xc1611@ic.ac.uk>
2013-11-12 21:13:42 +0000

    added tooltips for buttons

--- deeva/static/js/controllers.js
+++ deeva/static/js/controllers.js
@@ -288,5 +288,8 @@ deeva.controller('SimpleController', function ($scope, $http) {
     });
   }
    
-
+   $('.myButtons').tooltip('toggle');
+   $(document).ready(function() {
+    $('.myButtons').tooltip('hide');
+});
 });
