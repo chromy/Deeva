f9622b5ca0c8db0a78c169eac01ef58aedeb0b1e
Xueqi Chen <xc1611@ic.ac.uk>
2013-11-13 14:22:02 +0000

    fixed angular tooltip conflics

--- deeva/static/js/controllers.js
+++ deeva/static/js/controllers.js
@@ -292,9 +292,8 @@ deeva.controller('SimpleController', function ($scope, $http) {
       allowDuplicates: true,
     });
   }
-   
-   $('.myButtons').tooltip('toggle');
+
    $(document).ready(function() {
     $('.myButtons').tooltip('hide');
-});
+  });
 });
